f67bda1a476b3e3af3c50ed7da4818ce68d1df2d
kgd <kylegraydev@gmail.com>
2017-02-19 16:21:41 -0800

    Creates authentication, performs a basic tweet

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,7 @@
+class ShoutsController < ApplicationController
+
+  def show
+    @shout = Shout.find(params[:id])
+  end
+
+end
---
+++ test/controllers/text_shouts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TextShoutsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
